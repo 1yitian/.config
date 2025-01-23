34b76af1489e3da5751dbb91c6296af33705c421
Yiding Zhang <yitian@email.cn>
2025-01-23 13:28:43 +0800

    Don't map lsp functions when it is unnecessary

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -7,23 +7,30 @@ return {
 	},
 	config = function()
 		vim.api.nvim_create_autocmd('LspAttach', {
-			callback = function ()
-				map('n','gd',		vim.lsp.buf.definition,		'[g]oto [d]efinition')
-				map('n','gy',		vim.lsp.buf.type_definition,'[g]oto t[y]pe references')
-				map('n','gr',		vim.lsp.buf.references,		'[g]oto [r]eferences')
-				map('n','gi',		vim.lsp.buf.implementation,	'[g]oto [i]mplementation')
-				map('n','ge',		vim.lsp.buf.declaration,	'[g]oto d[e]claration')
+			callback = function()
+				local map = vim.keymap.set
+				local function getopt(desc)
+					return { desc = desc, buffer = 0 }
+				end
+				map('n','gd',		vim.lsp.buf.definition,		getopt('[g]oto [d]efinition'))
+				map('n','gy',		vim.lsp.buf.type_definition,getopt('[g]oto t[y]pe references'))
+				map('n','gr',		vim.lsp.buf.references,		getopt('[g]oto [r]eferences'))
+				map('n','gi',		vim.lsp.buf.implementation,	getopt('[g]oto [i]mplementation'))
+				map('n','ge',		vim.lsp.buf.declaration,	getopt('[g]oto d[e]claration'))
 
-				map('n','E',		vim.lsp.buf.hover,			'[h]over')
-				map('n','<leader>s',vim.lsp.buf.signature_help,	'[s]ignature help')
-				map('n','<leader>a',vim.lsp.buf.code_action,	'code [a]ction')
-				map('n','<leader>r',vim.lsp.buf.rename,			'[r]ename')
-				map('n','<leader>f',vim.lsp.buf.format,			'[f]ormat')
+				map('n','E',		vim.lsp.buf.hover,			getopt('[h]over'))
+				map('n','<leader>s',vim.lsp.buf.signature_help,	getopt('[s]ignature help'))
+				map('n','<leader>a',vim.lsp.buf.code_action,	getopt('code [a]ction'))
+				map('n','<leader>r',vim.lsp.buf.rename,			getopt('[r]ename'))
+				map('n','<leader>f',vim.lsp.buf.format,			getopt('[f]ormat'))
 			end
 		})
 		-- support nvim-cmp
 		local capabilities = require('cmp_nvim_lsp').default_capabilities();
-		local servers = {'lua_ls', 'clangd'}
+		local servers = {
+			'lua_ls',
+			'clangd'
+		}
 
 		-- Add lsps' path to $PATH
 		-- ':' is $PATH seperater for linux
