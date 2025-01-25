04ce11e226c93f2ba2b969d395145fd069dcd4ac
Yiding Zhang <yitian@email.cn>
2025-01-25 19:05:30 +0800

    Remove bkchrome.sh

--- bkchrome.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# name='chromium_backup_'$(date +'%Y_%m_%d_%H:%M:%S')
-rm -rf $(find ~/.config/chromium/Default -name "*cache*" -type d)
-rm -rf $(find ~/.config/chromium/Default -name "*.old")
-rm -rf $(find ~/.config/chromium/Default -name "*.log")
-tar -czvf ~/.config/chromium_profile.tar.gz ~/.config/chromium/Default/
-echo -e '\n\n~/.config/chromium_profile.tar.gz'
