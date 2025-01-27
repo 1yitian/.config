b287d9aac4464a24599fab2e1e04ea102193476e
Yiding Zhang <yitian@email.cn>
2025-01-27 17:25:25 +0800

    Custom nvim-notify settings

--- nvim/lua/plugins/nvim-notify.lua
+++ nvim/lua/plugins/nvim-notify.lua
@@ -1,7 +1,14 @@
 return {
 	'rcarriga/nvim-notify',
+	main = 'notify',
 	event = 'VeryLazy',
-	config = function()
-		vim.notify = require('notify');
+	opts = function()
+		vim.notify = require('notify')
+		return {
+			minimum_width = 26,
+			background_colour = '#000000',
+			render = 'minimal',
+			stages = 'static',
+		}
 	end
 }
